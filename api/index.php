cb8f79daa5b5607bc959b05e4979341bf0ec338c
harveyhans <harveyhansjavier@gmail.com>
2020-08-07 00:03:37 +0800

    baka baka

--- api/index.php
+++ api/index.php
@@ -7,23 +7,21 @@ $hash = $_GET["hash"];
 $key = $_GET["key"];
 $index = $_GET["index"];
 $innerkey = $_GET["innerkey"];
-$version = $_GET["diffhash"];
+$version = $_GET["version"];
 if(!isset($hash)){
 	die("");
 }
 
 $json = file_get_contents("https://bloodcat.com/osu/?mod=json&q=md5={$hash}");
 $result = json_decode($json, true)[0];
-if(!array_key_exists($key, $result)){
-	die("");
-}
 
 if(isset($index)){
 	if(isset($innerkey)) exit($result[$key][$index][$innerkey]);
 	exit($result[$key][$index]);
-}elseif(isset($diffhash)){
-	return array_filter($result["beatmaps"], fn($v) => $v["hash_md5"] == $diffhash);
+}elseif(isset($version) && $version == "hash"){
+	return array_filter($result["beatmaps"], fn($v) => $v["hash_md5"] == $hash);
 }elseif(isset($key)){
+	if(!array_key_exists($key, $result)) die("");
 	exit($result[$key]);
 }else{
 	die("");
